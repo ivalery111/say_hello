4b7c3f30d2cf0ac8150d7e4e4f0118479cf54479
Valery Ivanov <lavivanov@kraftway.ru>
2021-07-27 12:09:13 +0300

    chore(proj): split into header/source files

--- say_hello.c
+++ say_hello.c
@@ -1,7 +1,11 @@
+#include "say_hello.h"
+
 #include <stdio.h>
 
+void say_hello(void) { fprintf(stdout, "H3ll0 Fr13nd!\n"); }
+
 int main(void) {
-    fprintf(stdout, "H3ll0 Fr13nd!\n");
+    say_hello();
 
     return 0;
 }
